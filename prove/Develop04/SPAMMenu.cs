2e9e0b6795d066cdc9ca0ef02c900d0aa3b71012
AngeloAG <angelo.ag.079@gmail.com>
2023-02-23 14:19:31 -0700

    Added quit option

--- prove/Develop04/SPAMMenu.cs
+++ prove/Develop04/SPAMMenu.cs
@@ -19,26 +19,27 @@ Behaviors:
 Return user selected option
 */
 using System;
-public class SPAMMenu 
+public class SPAMMenu
 {
 
   public SPAMMenu()
   {
-    
+
   }
-  
+
   public void SPAMmenuSelection()
   {
     Console.WriteLine("Welcome to the Mindfulness Program. Please choose and activity to continue.");
     Console.WriteLine("1). Breathing Activity");
     Console.WriteLine("2). Reflection Activity");
     Console.WriteLine("3). Listing Activity");
+    Console.WriteLine("4). Quit");
   }
 
-  
+
   public string SPAMselection()
   {
     Console.Write("Please enter the numberical value of the preferred activity: ");
-    return Console.ReadLine();  
+    return Console.ReadLine();
   }
 }
